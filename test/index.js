dad876f9dbd9d128ee53fe0ffa838959df7f685a
Cheton Wu <cheton@gmail.com>
2018-01-04 10:43:01 +0800

    Setup tests

--- test/index.js
+++ test/index.js
@@ -1,5 +1,45 @@
+import React from 'react';
+import { mount } from 'enzyme';
 import { test } from 'tap';
+import '../setupTests';
+import { Button, ButtonGroup, ButtonToolbar } from '../src';
 
-test('noop', (t) => {
+test('<Button />', (t) => {
+    const wrapper = mount((
+        <Button>
+        </Button>
+    ));
+    t.equal(wrapper.find(Button).length, 1, 'should render <Button /> component');
+    t.end();
+});
+
+test('<ButtonGroup />', (t) => {
+    const wrapper = mount((
+        <ButtonGroup>
+            <Button></Button>
+            <Button></Button>
+        </ButtonGroup>
+    ));
+    t.equal(wrapper.find(ButtonGroup).length, 1, 'should render <ButtonGroup /> component');
+    t.equal(wrapper.find(Button).length, 2, 'should render <Button /> component');
+    t.end();
+});
+
+test('<ButtonToolbar />', (t) => {
+    const wrapper = mount((
+        <ButtonToolbar>
+            <ButtonGroup>
+                <Button></Button>
+                <Button></Button>
+            </ButtonGroup>
+            <ButtonGroup>
+                <Button></Button>
+                <Button></Button>
+            </ButtonGroup>
+        </ButtonToolbar>
+    ));
+    t.equal(wrapper.find(ButtonToolbar).length, 1, 'should render <ButtonToolbar /> component');
+    t.equal(wrapper.find(ButtonGroup).length, 2, 'should render <ButtonGroup /> component');
+    t.equal(wrapper.find(Button).length, 4, 'should render <Button /> component');
     t.end();
 });
